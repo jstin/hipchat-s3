64bfa4634e4a5c633b47ea53babc7532b6b614b8
Justin Derrek Van Eaton <jvaneaton@tangogroup.com>
2012-02-17 12:09:13 -0700

    Initial Commit

---
+++ Rakefile
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'rake'
+
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "hipchat-s3"
+    gem.summary = %Q{Ruby library to upload files to s3 and alert users in hipchat with a link}
+    gem.description = %Q{Ruby library to upload files to s3 and alert users in hipchat with a link}
+    gem.email = "jd@stinware.com"
+    gem.homepage = "http://github.com/jstin/hipchat-s3"
+    gem.authors = ["Justin"]
+    gem.add_dependency "hipchat"
+    gem.add_dependency "aws-s3"
+    gem.add_development_dependency "rspec", "~> 2.0"
+    gem.add_development_dependency "rr", "~> 1.0"
+  end
+  Jeweler::GemcutterTasks.new
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
+end
+
+task :default => :spec
+
+require 'rake/rdoctask'
+Rake::RDocTask.new do |rdoc|
+  version = File.exist?('VERSION') ? File.read('VERSION') : ""
+
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title = "hipchat-s3 #{version}"
+  rdoc.rdoc_files.include('README*')
+  rdoc.rdoc_files.include('lib/**/*.rb')
+end
---
+++ lib/hipchat-s3.rb
@@ -0,0 +1,45 @@
+require 'hipchat'
+require 'aws-s3'
+
+module HipchatS3
+
+  def initialize(options = {})
+
+    s3_creds = options.fetch(:s3, {:access_key_id => 'your-key', :secret_access_key => 'your-secret', :bucket => 'bucket'})
+    hipchat_creds = options.fetch(:hipchat, {:api_token => 'your-token'})
+
+    @s3_bucket = s3_creds.delete(:bucket)
+
+    @hipchat_client = HipChat::Client.new(hipchat_creds[:api_token])
+    AWS::S3::Base.establish_connection!(s3_creds)
+
+  end
+
+  def create_upload(path, room, username='fileuploader', message="File Uploaded", color='yellow')
+    unless tar_exists?
+      @hipchat_client[room].send(username, "You don't have tar installed on host", :notify => true, :color => color)
+      return
+    end
+
+    file = tar_with_path(path)
+    basename = File.basename(file)
+
+    AWS::S3::S3Object.store(basename, open(path), @s3_bucket)
+    @hipchat_client[room].send(username, "#{message} :: <a href=\"https://s3.amazonaws.com/#{@s3_bucket}/#{basename}\">#{basename}</a>", :notify => true, :color => color)
+  end
+
+
+private
+  
+  def tar_exists?
+    `which tar`.strip != ""
+  end
+
+  def tar_with_path(path)
+    cmd_path = `which tar`.strip
+    tarred_path = "#{path}.tar.gz"
+    `#{cmd_path} -czf #{tarred_path} #{path}`
+    tarred_path
+  end
+
+end
